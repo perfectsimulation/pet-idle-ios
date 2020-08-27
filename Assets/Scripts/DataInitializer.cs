245d745bd6d51d8df983ea7c96ae7ddd2a41ec67
Sally Blair <sally@sallyblair.io>
2020-08-27 07:57:10 -0700

    Stub guest

---
+++ Assets/Scripts/DataInitializer.cs
@@ -0,0 +1,7 @@
+﻿public static class DataInitializer
+{
+    public static readonly Guest GuestA = new Guest("Guest A", "Ambient", 0.2d, 2.2d, 20, 120);
+    public static readonly Guest GuestB = new Guest("Guest B", "Brash", 1d, 5d, 80, 80);
+    public static readonly Guest GuestC = new Guest("Guest C", "Charming", 0.4d, 4d, 55, 135);
+    public static readonly Guest GuestD = new Guest("Guest D", "Delightful", 5d, 9d, 11, 200);
+}
---
+++ Assets/Scripts/Guest.cs
@@ -0,0 +1,49 @@
+﻿public class Guest
+{
+    // Name of this Guest
+    public string Name;
+
+    // Personality of this Guest
+    public string Personality;
+
+    // Minutes before earliest arrival to yard
+    public double EarliestArrivalInMinutes;
+
+    // Minutes before latest arrival to yard
+    public double LatestArrivalInMinutes;
+
+    // Priority for tie-breaking guest selection
+    public int PowerLevel;
+
+    // Currency rewarded to Player upon completion of Guest visit
+    public int CurrencyRewardForVisit;
+
+    /* Default no-arg constructor */
+    public Guest()
+    {
+        this.Name = "Guest";
+        this.Personality = "Fun";
+        this.EarliestArrivalInMinutes = 0.1d;
+        this.LatestArrivalInMinutes = 1.1d;
+        this.PowerLevel = 0;
+        this.CurrencyRewardForVisit = 100;
+    }
+
+    /* Constructor */
+    public Guest(
+        string name,
+        string personality,
+        double earliestArrivalInMinutes,
+        double latestArrivalInMinutes,
+        int powerLevel,
+        int currencyRewardForVisit)
+    {
+        this.Name = name;
+        this.Personality = personality;
+        this.EarliestArrivalInMinutes = earliestArrivalInMinutes;
+        this.LatestArrivalInMinutes = latestArrivalInMinutes;
+        this.PowerLevel = powerLevel;
+        this.CurrencyRewardForVisit = currencyRewardForVisit;
+    }
+
+}
